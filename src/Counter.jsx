9da7964c41d8c6d5cdc0bdc58bbbc6ab8ba365eb
yuvaraj <yuvaraj@coursera-something.com>
2024-05-12 14:06:02 +0530

    CounterApp completed

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const handleIncrement = () => {
+        setCount((prevValue) => prevValue + 1);
+    }
+
+    const handleDecrement = () => {
+        setCount((prevValue) => prevValue - 1);
+    }
+  return (
+    <div>
+        <h1>Counter App</h1>
+        <p>Counter: {count} </p>
+        <div>
+            <button type="submit" onClick={handleIncrement}>Increment</button>
+            <button type="submit" onClick={handleDecrement}>Decrement</button>
+        </div>
+    </div>
+  )
+}
+
+export default Counter
